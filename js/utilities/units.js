448219e17d8dbe01a4d03a27aae0635544f270d0
dariomauleon <mauleondario@gmail.com>
2025-06-19 14:37:15 -0600

    utilities.js agregado

---
+++ js/utilities/units.js
@@ -0,0 +1,7 @@
+export const units = {
+  length: ["m", "km", "cm", "mm", "in", "ft", "yd", "mi"],
+  weight: ["kg", "g", "mg", "lb", "oz"],
+  time: ["s", "min", "h", "day"],
+  speed: ["m/s", "km/h", "mph", "ft/s"],
+  temperature: ["C", "F", "K"],
+};
