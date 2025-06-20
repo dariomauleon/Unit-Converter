66f7a95b0669fcbe86f4e829ede454dc211a6bf6
dariomauleon <mauleondario@gmail.com>
2025-06-20 15:46:53 -0600

    fixed solucionado

--- js/modules/converters.js
+++ js/modules/converters.js
@@ -2,20 +2,27 @@ import { units, unitTable } from "../utilities/index.js";
 import { createLabelOutput, cleanInputs } from "./ui.js";
 
 const validations = (category, fromUnit, toUnit, valueInput) => {
-  if (category === "" || !Object.keys(units).includes(category)) {
-    alert("La categoría debe existir en la gama de opciones");
+  [category, fromUnit, toUnit, valueInput].forEach((el) => {
+    el.nextElementSibling?.classList.remove("show");
+  });
+
+  if (category.value === "" || !Object.keys(units).includes(category.value)) {
+    category.nextElementSibling.classList.add("show");
     return false;
   }
-  if (fromUnit === "" || !units[category].includes(fromUnit)) {
-    alert("Unidad fuera de la gama");
+  if (
+    fromUnit.value === "" ||
+    !units[category.value].includes(fromUnit.value)
+  ) {
+    fromUnit.nextElementSibling.classList.add("show");
     return false;
   }
-  if (toUnit === "" || !units[category].includes(toUnit)) {
-    alert("Unidad fuera de la gama");
+  if (toUnit.value === "" || !units[category.value].includes(toUnit.value)) {
+    toUnitUnit.nextElementSibling.classList.add("show");
     return false;
   }
-  if (valueInput === "" || isNaN(valueInput)) {
-    alert("El valor debe ser un número");
+  if (valueInput.value === "" || isNaN(valueInput.value)) {
+    valueInput.nextElementSibling.classList.add("show");
     return false;
   }
   return true;
@@ -33,7 +40,10 @@ const handleConverter = ({
   const toUnit = $unitSelectorTo.value;
   const inputValue = $inputValue.value;
 
-  if (!validations(category, fromUnit, toUnit, inputValue)) return;
+  if (
+    !validations($unitCategory, $unitSelectorFrom, $unitSelectorTo, $inputValue)
+  )
+    return;
 
   let result;
 
@@ -42,7 +52,7 @@ const handleConverter = ({
   } else {
     result = generalConverter(category, fromUnit, toUnit, inputValue);
   }
-  createLabelOutput(result, category, toUnit);
+  createLabelOutput(result, toUnit, $outputValue);
   cleanInputs({
     $unitCategory,
     $unitSelectorFrom,
